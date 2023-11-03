f44c6493ddce02974f8da44fb4a1a6bdc3a77161
ValorUBK <949685882@qq.com>
2023-11-03 15:43:19 +0800

    first init

---
+++ src/test/java/com/valorubk/designpatternprac/DesignPatternPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valorubk.designpatternprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
