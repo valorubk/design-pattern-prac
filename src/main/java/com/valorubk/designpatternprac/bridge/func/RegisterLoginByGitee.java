c85c9f7ad7088a9b2d3c144594fe779ca0fe1eb3
Giraffer <949685882@qq.com>
2024-03-27 19:04:24 +0800

    user login in bridge pattern (part)

--- src/main/java/com/valorubk/designpatternprac/bridge/func/RegisterLoginByGitee.java
+++ src/main/java/com/valorubk/designpatternprac/bridge/func/RegisterLoginByGitee.java
@@ -1,10 +1,26 @@
 package com.valorubk.designpatternprac.bridge.func;
 
 import com.valorubk.designpatternprac.pojo.UserInfo;
+import com.valorubk.designpatternprac.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
-public class RegisterLoginByGitee implements RegisterLoginFuncInterface{
+public class RegisterLoginByGitee implements RegisterLoginFuncInterface {
+
+    @Value("${gitee.state}")
+    private String giteeState;
+    @Value("${gitee.token.url}")
+    private String giteeTokenUrl;
+    @Value("${gitee.user.url}")
+    private String giteeUserUrl;
+    @Value("${gitee.user.prefix}")
+    private String giteeUserPrefix;
+
+    @Resource
+    private UserRepository userRepository;
+
     @Override
     public String login(String account, String password) {
         return null;
