190ad70f24d5cd8e152e0090e8156fe77da12ec2
Giraffer <949685882@qq.com>
2024-03-27 21:05:56 +0800

    补全适配器模式的代码

--- src/main/java/com/valorubk/designpatternprac/controller/UserController.java
+++ src/main/java/com/valorubk/designpatternprac/controller/UserController.java
@@ -1,19 +1,20 @@
 package com.valorubk.designpatternprac.controller;
 
+import com.valorubk.designpatternprac.adapter.Login3rdAdapter;
 import com.valorubk.designpatternprac.pojo.UserInfo;
 import com.valorubk.designpatternprac.service.UserService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.io.IOException;
 
 @RestController
 public class UserController {
 
     @Resource
     private UserService userService;
+    @Resource
+    private Login3rdAdapter login3rdAdapter;
 
     @PostMapping("/login")
     public String login(String account, String password) {
@@ -23,5 +24,10 @@ public class UserController {
     @PostMapping("/register")
     public String register(@RequestBody UserInfo userInfo) {
         return userService.register(userInfo);
+    }
+
+    @GetMapping("/gitee")
+    public String gitee(String code, String state) throws IOException {
+        return login3rdAdapter.loginByGitee(code, state);
     }
 }
